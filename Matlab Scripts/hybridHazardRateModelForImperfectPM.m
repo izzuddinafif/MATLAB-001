d8893753b311eef9bb527eb72b2902d8d8f81824
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2022-12-04 18:24:16 +0700

    revision

--- Matlab Scripts/hybridHazardRateModelForImperfectPM.m
+++ Matlab Scripts/hybridHazardRateModelForImperfectPM.m
@@ -1,11 +1,14 @@
 prompt="Input N: ";
+r=0.9;
+x=40;
+y=2.5;
 N=input(prompt)
-for k=1:i
+for k=1:N
     if k==1
         A(k)=0;
         B(k)=1;
-        T(k)=(40*(((A(k)/40)^2.5-(log(0.9)/B(k)))^(1/2.5)))-A(k);
-        T(k)=round(T(k), 4);
+        T(k)=(x*(((A(k)/x)^y-(log(r)/B(k)))^(1/y)))-A(k);
+        T(k)=round(T(k), N);
         continue
     end
     g=k-1;
@@ -13,7 +16,7 @@ for k=1:i
     A(k)=A(k-1)+(T(k-1)*ak(k));
     bk(k)=(4*g+1)/(3*g+1);
     B(k)=B(k-1)*bk(k);
-    T(k)=(40*(((A(k)/40)^2.5-(log(0.9)/B(k)))^(1/2.5)))-A(k);
-    T(k)=round(T(k), 4);
+    T(k)=(x*(((A(k)/x)^y-(log(r)/B(k)))^(1/y)))-A(k);
+    T(k)=round(T(k), N);
 end
 T
